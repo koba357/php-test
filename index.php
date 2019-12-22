991f318f50bcce50a25b0371711507d988f8cc4c
koba357 <khiroaki1001@gmail.com>
2019-12-22 17:28:44 +0900

    add image/fix index.php
    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-// 秒 sec
-echo date("Y-m-d H:i:s") ." 現在\n";
-echo date("Y-m-d H:i:s", strtotime("-3 sec")) ." 3秒前\n";
-echo date("Y-m-d H:i:s", strtotime("-2 sec")) ." 2秒前\n";
-echo date("Y-m-d H:i:s", strtotime("-1 sec")) ." 1秒前\n";
-echo date("Y-m-d H:i:s", strtotime("1 sec")) ." 1秒後\n";
-echo date("Y-m-d H:i:s", strtotime("2 sec")) ." 2秒後\n";
-echo date("Y-m-d H:i:s", strtotime("3 sec")) ." 3秒後\n\n";
+header('Content-type: image/jpeg');
+readfile('image/note.jpeg');
 ?>
